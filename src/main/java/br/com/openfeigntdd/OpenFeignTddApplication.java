650062a34ccba3def8354f8a04792c94089c6252
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2023-09-28 23:34:29 -0300

    Initial commit

---
+++ src/main/java/br/com/openfeigntdd/OpenFeignTddApplication.java
@@ -0,0 +1,13 @@
+package br.com.openfeigntdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFeignTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenFeignTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/openfeigntdd/OpenFeignTddApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.openfeigntdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenFeignTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
